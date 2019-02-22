c5b5029d67580f15430def674386c0482b335b3a
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-02-22 20:02:48 +0100

    n08SmartCasts.kt reformat

--- src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
@@ -11,10 +11,10 @@ class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 
 fun eval(e: Expr): Int =
-    when (e) {
-        is Num -> e.value
-        is Sum -> eval(e.left) + eval(e.right)
-    }
+        when (e) {
+            is Num -> e.value
+            is Sum -> eval(e.left) + eval(e.right)
+        }
 
 fun todoTask8(expr: Expr): Nothing = TODO(
     """
