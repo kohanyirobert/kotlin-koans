192f8766a6b88fe84e0eca186bbfc61c75359eb3
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-02-23 06:05:37 +0100

    n17MaxMin.kt done

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -5,12 +5,5 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
-    // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
-}
-
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
-    // Return the most expensive product which has been ordered
-    todoCollectionTask()
-}
+fun Shop.getCustomerWithMaximumNumberOfOrders() = customers.maxBy { it.orders.count() }
+fun Customer.getMostExpensiveOrderedProduct() = orderedProducts.maxBy { it.price }
